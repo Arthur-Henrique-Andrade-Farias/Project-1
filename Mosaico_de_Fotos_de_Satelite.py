81a2239e55658edc681a4a1daf219eb2d0e64210
arthurHAF <101343526+arthurHAF@users.noreply.github.com>
2022-06-01 15:49:27 -0300

    Mosaico_de_fotos_de_satelite_2.py
    For some reasons the work changed for 1 only rectangle surrounding all others.

--- Mosaico_de_Fotos_de_Satelite.py
+++ Mosaico_de_Fotos_de_Satelite.py
@@ -1,19 +1,17 @@
-r = 0
-xir = 0
-yir = 0
-xfr = 0
-yfr = 0
-x = "sim"
-while x == "sim":
-    npr = int(input())
-    for t in range(npr):
-        xi,yi,xf,yf = map(int,input().split())
-        xi1,yi1,xf1,yf1 = map(int, input().split())
-        if xi1 >= xi and xi1 <= xf1 and yi1 <= yi and yi1 >= yf and xf1 > xf and yf1 <= yi and yf1 >= yf:
-            xfr = xf1
-            yfr = yf
-            yir = yi
-            xir = xi
-            r += 1
-    print("{}\n({},{}) ({},{})".format(r,xir, yir, xfr, yfr))
-    x = input("Deseja adicionar mais alguma área?(sim ou nao)\n")
+with open("Gerador.txt", "r") as arquivo:
+    xse, yse, xid, yid = map(int, arquivo.readline().split())
+    while True:
+        try:
+            xs1, ys1, xi1, yi1 = map(int, arquivo.readline().split())
+            xsf = min(xs1,xse)
+            ysf = max(ys1,yse)
+            xif = max(xi1,xid)
+            yif = min(yi1,yid)
+            xse = xsf 
+            yse = ysf 
+            xid = xif 
+            yid = yif
+        except ValueError:
+            break
+    print('1')
+    print("({},{}), ({},{})".format(xse, yse, xid, yid))
